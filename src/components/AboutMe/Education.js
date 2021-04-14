a3fe5951622b9e71ed8317a63198f27638508dea
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-04-14 08:26:38 -0700

    Adds Education display

--- src/components/AboutMe/Education.js
+++ src/components/AboutMe/Education.js
@@ -3,8 +3,47 @@ import { AboutMeNav } from "./AboutMeNav";
 
 const Education = () => {
   return (
-    <section>
-      <span>Education Section</span>
+    <section className="aboutme_container">
+      <section className="education_card_container">
+        <section className="education_card">
+          <div>
+            <span className="span1">
+              Master of Science, Software Engineering
+            </span>
+          </div>
+          <div>
+            <span className="span2">
+              California State University, Fullerton
+            </span>
+          </div>
+          <div>
+            <span className="span3">June 2006</span>
+          </div>
+          <div className="csuf_image_container">
+            <div className="csuf_image">
+              <img src="/images/Techs/CSUF_2.jpg" alt="CSUF" />
+            </div>
+          </div>
+        </section>
+        <section className="education_card">
+          <div>
+            <span className="span1">Bachelor of Science, Computer Science</span>
+          </div>
+          <div>
+            <span className="span2">
+              California State University, Fullerton
+            </span>
+          </div>
+          <div>
+            <span className="span3">June 2003</span>
+          </div>
+          <div className="csuf_image_container">
+            <div className="csuf_image">
+              <img src="/images/Techs/CSUF_2.jpg" alt="CSUF" />
+            </div>
+          </div>
+        </section>
+      </section>
       <AboutMeNav />
     </section>
   );
