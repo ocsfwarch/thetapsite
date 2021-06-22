6dfcf46a0d49d1163eb654ac9189f40be63e984c
ocsfwarch <38299239+ocsfwarch@users.noreply.github.com>
2021-06-22 13:30:01 -0700

    Update app_header_banner
    Modified the banner so that it maintains a fixed height, removed the <h1> and set the font-size and font-weight.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import { NavBar } from './NavBar';
+import React from "react";
+import { NavBar } from "./NavBar";
 
 export const Header = () => {
-
-    return(
-        <div className="app_header">
-            <div className="app_header_banner">
-                <section ><h1>The TAP Site</h1></section>
-            </div>
-            <div className={"app_header_menu"}>
-                <NavBar />
-            </div>
-        </div>
-    );
+  return (
+    <div className="app_header">
+      <div className="app_header_banner">The TAP Site</div>
+      <div className={"app_header_menu"}>
+        <NavBar />
+      </div>
+    </div>
+  );
 };
